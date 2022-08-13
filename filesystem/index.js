b6e9f89fd2cece0671d54cdffb42f66374da9908
Syarif Maulana <syarifmaulana.id@gmail.com>
2022-08-13 19:21:47 +0700

    ya

---
+++ filesystem/index.js
@@ -0,0 +1,16 @@
+// TODO: tampilkan teks pada notes.txt pada console.
+
+// Include fs module
+const fs = require('fs');
+const path = require('path');
+
+const fileReadCallback = (error, data) => {
+    if(error) {
+        console.log('Gagal membaca berkas');
+        return;
+    }
+console.log(data);
+}
+
+
+fs.readFile(path.resolve(__dirname, 'notes.txt'), 'UTF-8', fileReadCallback);
